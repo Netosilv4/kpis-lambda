76c6e5484361dc187255425efe661e3d4a14309b
Netosilv4 <netosilv44455@gmail.com>
2022-09-12 21:32:22 -0300

    fix: multer aws lambda

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import cors from 'cors'
 import authRoutes from './modules/auth/routes';
 import { errorMiddleware } from './middlewares/errorHandler';
 import chartRoutes from './modules/charts/routes';
-import multer from 'multer';
-import { populateHandler } from './modules/populate';
+// import multer from 'multer';
+// import { populateHandler } from './modules/populate';
 
-const upload = multer({ dest: 'tmp/uploads/' });
+// const upload = multer({ dest: 'tmp/uploads/' });
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(cors());
 
-app.post("/populate", upload.single("file"), populateHandler);
+// app.post("/populate", upload.single("file"), populateHandler);
 
 app.use(chartRoutes)
 
